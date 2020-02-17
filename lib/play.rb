d63c3b923017e0c38b63f44f1a67abf5d04b07c4
dazzling-directory-8519 <yamirabasabe@gmail.com>
2020-02-17 12:52:07 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,6 +1,6 @@
 # Helper Methods
 def display_board('board')
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{'board[0]'} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
